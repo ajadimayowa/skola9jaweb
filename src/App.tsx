064b93fcd850af02d125434f11233a1b113b3ed6
ajadimayowa <samtalktech@outlook.com>
2022-12-24 01:05:29 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="success">Login</Button>
+    </div>
+  );
+}
+
+export default App;
