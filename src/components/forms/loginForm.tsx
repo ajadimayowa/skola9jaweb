120008a880c30042656545e719b16663576a0d5a
ajadimayowa <samtalktech@outlook.com>
2022-12-28 09:22:03 -0800

    handle input error

--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -11,6 +11,7 @@ const LoginForm = () => {
             </FormGroup >
             <FormGroup className="m-3 mb-5">
                 <PrimaryInput placeholder={'Password'} />
+                <p className="mt-2" style={{color:'#FF8B8B'}}>{}</p>
             </FormGroup >
             <FormGroup className="d-flex m-3 justify-content-center">
                 <PrimaryButton>Login</PrimaryButton>
