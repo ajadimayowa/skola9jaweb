44f441eb7a28c9f0e6900df52145bac697748bb0
ajadimayowa <samtalktech@outlook.com>
2022-12-28 15:15:30 -0800

    Login Authentication done

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 const PrimaryButton = (props: any) => {
+    const {onClick} = props 
     return (
-        <Button style={{outlineWidth:0, backgroundColor : '#E79C3D', border:0, minWidth:200, borderRadius:50}}>{props.children}</Button>
+        <Button type='submit' onClick={props.onClick} style={{outlineWidth:0, backgroundColor : '#E79C3D', border:0, minWidth:200, borderRadius:50}}>{props.children}</Button>
         
     )
 
--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -1,23 +1,43 @@
 import React from "react";
-import { Form, FormGroup } from "react-bootstrap";
+import { Alert, Form, FormGroup } from "react-bootstrap";
 import PrimaryButton from "../buttons/PrimaryButton";
 import PrimaryInput from "../inputs/primaryInput";
+import { useFormik } from "formik";
+import * as yup from 'yup';
 
 const LoginForm = () => {
+    
+
+    const formik = useFormik({
+        initialValues: { username: '', password: '' },
+        validationSchema: yup.object({}),
+        onSubmit: (value) => handleLogin(value)
+    })
+
+    const user = { validName: 'ade', validPassword: 'tola' }
+
+    const handleLogin = (val: any) => {
+        if (user.validName === val.username && user.validPassword === val.password) {
+            console.log('Log In Succesful')
+        } 
+        else alert('Not a registered User')
+
+    }
+
     return (
         <Form>
-            
+
             <FormGroup className="d-flex m-3 justify-content-center">
-                <PrimaryInput placeholder={'Username'} />
+                <PrimaryInput handleChange={formik.handleChange} name={'username'} inputValue={formik.values.username} placeholder={'Username'} />
             </FormGroup >
             <FormGroup className="d-flex m-3 justify-content-center">
-                <PrimaryInput placeholder={'Password'} />
+                <PrimaryInput handleChange={formik.handleChange} name={'password'} placeholder={'Password'} />
                 <p className="mt-2" style={{ color: '#FF8B8B' }}>{ }</p>
             </FormGroup >
             <FormGroup className="d-flex m-3 justify-content-center">
-                <PrimaryButton>Login</PrimaryButton>
+                <PrimaryButton onClick={formik.handleSubmit}>Login</PrimaryButton>
             </FormGroup >
-           
+
         </Form>
     )
 }
